d436437b87da9efa0d8d6af4a6dc4b26bd0ab5ff
Timur Usmanov <t.usmanov@innopolis.university>
2026-06-15 15:19:11 +0300

    WIP fixing RegexMinusSpec

--- source/test/BNFC/RegexMinusSpec.hs
+++ source/test/BNFC/RegexMinusSpec.hs
@@ -4,24 +4,62 @@ import Test.Hspec
 
 import BNFC.RegexMinus
 
--- | lists remaining suffixes greedily
-match :: Regex Char -> String -> [String]
+-- | lists matched prefix lengths in STRICTLY ASCENDING order
+match :: Regex Char -> String -> [Int]
 match = \case
     Term ch -> \case
-        h:suf -> if h == ch then [suf] else []
+        h:_ -> if h == ch then [1] else []
         _ -> []
-    Lambda -> (:[])
+    Lambda -> const [0]
     Phi -> const []
-    Rep reg -> helper
+    Rep reg -> helper 0 []
         where
-            matcher = match reg
-            helper s = concatMap helper (matcher s) ++ [s]
-    Or a b -> \s -> match a s ++ match b s
-    Seq a b -> concatMap (match b) . match a
+            helper dropped todo s =
+                let matches = map (+dropped) $ matchNonempty reg s
+                    newtodo = merge todo matches
+                in dropped : case newtodo of
+                    [] -> []
+                    nx:tail -> helper nx tail $ drop (nx - dropped) s
+            matchNonempty r s = case match r s of
+                0:tail -> tail
+                other -> other
+    Or a b -> \s -> match a s `merge` match b s
+    Seq a b -> \s -> let
+            amatches = match a s
+            amatchessuf = zip amatches $ sufs amatches s
+        in mergeMany [map (+dropped) $ match b s | (dropped, s) <- amatchessuf]
     Sub _ _ -> error "No Sub promised, but one encountered"
+    where
+        merge :: [Int] -> [Int] -> [Int]
+        merge = \case
+            [] -> id
+            as@(a:at) -> \case
+                [] -> as
+                bs@(b:bt) -> if a < b then a : merge at bs else
+                    if a > b then b : merge as bt else
+                    a : merge at bt
+        mergeMany :: [[Int]] -> [Int]
+        mergeMany = foldr merge []
+        sufs :: [Int] -> String -> [String]
+        sufs = helper 0
+            where
+                helper prevlen lens prevsuf = case lens of
+                    [] -> []
+                    len:tail -> let newsuf = drop (len - prevlen) prevsuf in
+                        newsuf : helper len tail newsuf
 
 matchFull :: Regex Char -> String -> Bool
-matchFull reg s = any null $ match reg s
+matchFull reg s = elem (length s) $ match reg s
+
+trueRemoveMinuses :: Eq a => Regex a -> Regex a
+trueRemoveMinuses = \case
+    t@(Term _) -> t
+    l@Lambda -> l
+    p@Phi -> p
+    Rep r -> Rep $ trueRemoveMinuses r
+    Or a b -> Or (trueRemoveMinuses a) (trueRemoveMinuses b)
+    Sub a b -> removeMinuses $ Sub (trueRemoveMinuses a) (trueRemoveMinuses b)
+    Seq a b -> Seq (trueRemoveMinuses a) (trueRemoveMinuses b)
 
 spec :: Spec
 spec = do
@@ -29,6 +67,17 @@ spec = do
             = it ((if null s then "Empty" else s)
                 ++ (if expected then " matches" else " does not match"))
                 $ function s `shouldBe` expected
+
+    describe "self-check" $ do
+        let exp = Rep (Term 'a')
+            test = testcase (matchFull exp)
+        test "" True
+        test "a" True
+        test "aaA" False
+        test "b" False
+        test "aaaaaab" False
+        test "aaaaaaa" True
+        test "baaaaaa" False
 
     -- Example that requires loop detection. Taken From:
     --  http://home.chello.no/~mgrsby/sgmlintr/file0005.htm
@@ -158,3 +207,51 @@ spec = do
         test "ba" True
         test "_" False
         test "a d" False
+
+    describe ("[a-z]* - [aeiouy]* = " ++ (compactShow $ removeMinuses
+                $ Sub (Rep (charset "abcdefghijklmnopqrstuvwxyz"))
+                      (Rep (charset "aeiouy")))) $ do
+        let exp = removeMinuses
+                $ Sub (Rep (charset "abcdefghijklmnopqrstuvwxyz"))
+                      (Rep (charset "aeiouy"))
+            test = testcase (matchFull exp)
+        test "" False
+        test "abcd" True
+        test "aaa" False
+        test "bcd" True
+        test "xfsckhzbtlpmgnrvqjwd" True
+        test "yoaueaiueaieiau" False
+        test "xfsckhzbtlpmagnrvqjwd" True
+        test "yoaueaiueaiveiau" True
+
+    describe "([a-z]* - [aeiouy]*) - bnfc" $ do
+        let exp = removeMinuses
+                $ Sub (Sub (Rep (charset "abcdefghijklmnopqrstuvwxyz"))
+                           (Rep (charset "aeiouy")))
+                      (string "bnfc")
+            test = testcase (matchFull exp)
+        test "" False
+        test "abcd" True
+        test "aaa" False
+        test "bcd" True
+        test "xfsckhzbtlpmgnrvqjwd" True
+        test "yoaueaiueaieiau" False
+        test "xfsckhzbtlpmagnrvqjwd" True
+        test "yoaueaiueaiveiau" True
+        test "bnfc" False
+        test "nfc" True
+        test "bnf" True
+
+    describe "[a-z]* - ([bcdfghjklmnpqrstvwxz]* - bnfc)" $ do
+        let exp = removeMinuses
+                $ Sub (Rep (charset "abcdefghijklmnopqrstuvwxyz"))
+                      (Sub (Rep (charset "bcdfghjklmnpqrstvwxz"))
+                           (string "bnfc"))
+            test = testcase (matchFull exp)
+        test "" False
+        test "abcd" True
+        test "bcd" False
+        test "xfsckhzbtlpmgnrvqjwd" False
+        test "bnfc" True
+        test "nfc" False
+        test "bnf" False
