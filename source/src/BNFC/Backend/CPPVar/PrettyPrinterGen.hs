b4c305c7e015995ca7cf80f811998afbb00d8589
Timur Usmanov <t.usmanov@innopolis.university>
2026-06-28 14:24:54 +0300

    Add [[maybe_unused]] to PrettyPrinter.cpp::INDENT

--- source/src/BNFC/Backend/CPPVar/PrettyPrinterGen.hs
+++ source/src/BNFC/Backend/CPPVar/PrettyPrinterGen.hs
@@ -103,7 +103,7 @@ printerUtilImpl = unlinesToText [s|
 #define IF_BAD_COERC(classname) \
     if (coercionLevel > reflection::CoercionLevel<classname>)
 
-constexpr static const unsigned int INDENT = 4;
+constexpr static const unsigned int INDENT [[maybe_unused]] = 4;
 
 PrettyPrinter::PrettyPrinter(std::ostream& out, unsigned int indent,
                              int coercionLevel)
