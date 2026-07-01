272afd0832297fe5479e2bd2bad8d948d55593fa
Timur Usmanov <t.usmanov@innopolis.university>
2026-07-01 12:45:03 +0300

    Follow style guide in BNFC.Backend.CPPVar (the main module)

--- source/src/BNFC/Backend/CPPVar.hs
+++ source/src/BNFC/Backend/CPPVar.hs
@@ -1,13 +1,20 @@
-{-
-  BNF Converter -> C++17 with std::variant
-  (C) (2026) Author: Timur Usmanov <t.usmanov@innopolis.university>
+{-|
+  Module      : BNFC.Backend.CPPVar
+  Description : C++17 backend using FLex, Bison, and the standard template
+                library (especially std::variant).
+  Maintainer  : Timur Usmanov <t.usmanov@innopolis.university>
 -}
 
-module BNFC.Backend.CPPVar (makeCppVar) where
+module BNFC.Backend.CPPVar
+  (
+    -- * The entrypoint
+    makeCppVar
+  ) where
 
 import BNFC.CF
 import BNFC.Options
 import BNFC.Backend.Base
+
 import qualified BNFC.Backend.CPPVar.CPPUtil as CPPUtil
 import BNFC.Backend.CPPVar.AbsynGen
 import BNFC.Backend.CPPVar.FlexGen
@@ -20,10 +27,44 @@ import BNFC.Backend.CPPVar.TestGen
 import BNFC.Backend.CPPVar.PrinterCommonGen
 import BNFC.Backend.CPPVar.MakefileGen
 
-comment :: String -> String
-comment = ("/* "++) . (++" */")
+{-| Generates the following files (@language@ is the basename of the LBNF
+grammar description file, as given by 'BNFC.Options.lang'):
 
-makeCppVar :: SharedOptions -> CF -> MkFiles ()
++----------------------------+-------------------------------------------+
+|            FILE            |                DESCRIPTION                |
++============================+===========================================+
+| @Absyn.hpp@                | abstract syntax nodes declarations        |
++----------------------------+-------------------------------------------+
+| @PatternMatching.hpp@      | the pipe operator overload                |
++----------------------------+-------------------------------------------+
+| @PrinterCommon.hpp@        | common string visualization utilities     |
++----------------------------+-------------------------------------------+
+| @PrettyPrinter.hpp@        | pretty-printing                           |
++----------------------------+-------------------------------------------+
+| @SyntaxPrinter.hpp@        | visualization of the abstract syntax tree |
++----------------------------+-------------------------------------------+
+| @language.l@               | the FLex lexer definition                 |
++----------------------------+-------------------------------------------+
+| @language.ypp@             | the Bison parser definition               |
++----------------------------+-------------------------------------------+
+| @Makefile@ (if @-m@ given) | recipes reference                         |
++----------------------------+-------------------------------------------+
+| @Absyn.cpp@                |                                           |
++----------------------------+-------------------------------------------+
+| @PrinterCommon.cpp@        |                                           |
++----------------------------+-------------------------------------------+
+| @PrettyPrinter.cpp@        |                                           |
++----------------------------+-------------------------------------------+
+| @SyntaxPrinter.cpp@        |                                           |
++----------------------------+-------------------------------------------+
+| @Test.cpp@                 | an example parser, used for testing       |
++----------------------------+-------------------------------------------+
+-}
+
+makeCppVar ::
+     SharedOptions  -- ^ BNFC invokation options.
+  -> CF             -- ^ The grammar description.
+  -> MkFiles ()
 makeCppVar opts cf = do
   let
     groupedRules = CPPUtil.groupRules cf
@@ -46,5 +87,9 @@ makeCppVar opts cf = do
   mkfile prettyPrinterCppFilename comment prettyCpp
   mkfile testFilename comment (makeTest opts)
   case optMake opts of
-    Nothing -> return ()
+    Nothing           -> return ()
     Just makefileName -> mkfile makefileName ("# " ++) (makeMakefile opts)
+
+-- | C++ comment wrapper.
+comment :: String -> String
+comment = ("/* " ++) . (++ " */")
